8d840e98d033ae4f9d48320a4783a4f18d484021
GUchechi <godswilluchendu7@gmail.com>
2022-08-13 03:19:20 +0100

    added form conditional validation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
 
+const isLoggedIn = false;
+
+const renderConditionally = () => {
+  if(!isLoggedIn) {
+    return (
+        <form className="form">
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+      </form>
+    )
+  } else{
+    return (
+      <h1>Hello</h1>
+    )
+  }
+}
+
 function App() {
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
+        {
+          renderConditionally()
+        }
     </div>
   );
 }
