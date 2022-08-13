5519405d1c7811463608e7bc6d940cb8258a5585
GUchechi <godswilluchendu7@gmail.com>
2022-08-13 03:54:44 +0100

    separated concern and added various components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
 import Login from './Components/Login';
 
-const isLoggedIn = false;
-
-const renderConditionally = () => {
-  if(!isLoggedIn) {
-    return <Login />
-  } else{
-    return (
-      <h1>Hello</h1>
-    )
-  }
-}
+const isLoggedIn = true;
 
 function App() {
   return (
     <div className="container">
-        {renderConditionally()}
+      {
+        !isLoggedIn === true ? <Login /> : <h1>Hello</h1>
+      }
     </div>
   );
 }
