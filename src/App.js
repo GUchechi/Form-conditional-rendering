9ba2c27563b30f81c3d1b5c7e55292814fbfd492
GUchechi <godswilluchendu7@gmail.com>
2022-08-13 02:49:14 +0100

    added a form

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hello</h1>
+      <form className="form">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
